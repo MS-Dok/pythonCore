1f1f98408bb23954cf6e5d55200bc7e939823fd0
MS-Dok <dmytro.poltavskyi@gmail.com>
2019-05-14 20:45:06 +0300

    Update 5.2 No yelling.py

--- 5.2 No yelling.py
+++ 5.2 No yelling.py
@@ -1,3 +1,8 @@
+'''
+Write a function taking in a string like WOW this is REALLY amazing and returning Wow this is really amazing. 
+String should be capitalized and properly spaced. Using re and string is not allowed.
+'''
+
 def filter_words(st):
   return(" ".join(str(st[0].upper()+st[1:len(st)+1].lower()).split()))
   
