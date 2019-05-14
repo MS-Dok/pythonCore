38ea58369a6f20b0d4f0606e1c32feab4922f0cd
MS-Dok <dmytro.poltavskyi@gmail.com>
2019-05-14 20:44:30 +0300

    Update 5.4_sticks.py

--- 5.4_sticks.py
+++ 5.4_sticks.py
@@ -1,2 +1,6 @@
+'''
+Create a robot that will always win the game. Your robot will always go first. 
+The function should take an integer and returns 1, 2, or 3.
+'''
 def make_move(sticks):
     return sticks%4 if sticks>=3 else sticks
