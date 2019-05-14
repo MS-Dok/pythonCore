60303a4624aba8a9a154aa1884eadb02969d1dba
MS-Dok <dmytro.poltavskyi@gmail.com>
2019-05-14 20:51:40 +0300

    Update banjo.py

--- banjo.py
+++ banjo.py
@@ -1,2 +1,6 @@
+'''
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+'''
 def areYouPlayingBanjo(name):
     return (name+" plays banjo" if name[0].lower()=="r" else name+" does not play banjo")
